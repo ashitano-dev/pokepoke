bb4a2d7acf9067c7bdda6f0914bb41e93df68111
koutyuke <gunji.kousuke08@gmail.com>
2025-03-16 20:40:09 +0900

    ✨ feat(tools/mobile): add expo application

---
+++ apps/mobile/tailwind.config.ts
@@ -0,0 +1,20 @@
+import { baseConfig } from "@pokepoke/tailwind-config";
+import type { Config } from "tailwindcss";
+
+const nativewindConfig = require("nativewind/preset");
+
+const config = {
+	content: {
+		files: [
+			"./src/**/*.{js,jsx,ts,tsx}",
+			// "../../packages/ui/src/**/*.{js,jsx,ts,tsx}",
+		],
+	},
+	presets: [nativewindConfig, baseConfig],
+	corePlugin: {
+		textOpacity: true,
+		backgroundOpacity: true,
+	},
+} satisfies Config;
+
+export default config;
