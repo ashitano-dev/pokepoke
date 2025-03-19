3a35c78d27905be1120d5472b27dcb274a238ea9
koutyuke <gunji.kousuke08@gmail.com>
2025-03-19 19:14:00 +0900

    ✨ feat(backend/use-cases): implement GetUserFriends use case to retrieve user's friends

--- apps/backend/src/application/use-cases/friend/index.ts
+++ apps/backend/src/application/use-cases/friend/index.ts
@@ -1,5 +1,7 @@
 export { CreateFriendInviteTokenUseCase } from "./create-friend-invite-token.usecase";
 export { ApplyFriendUseCase } from "./apply-friend.usecase";
+export { GetUserFriendsUseCase } from "./get-user-friends";
 
 export type { ICreateFriendInviteTokenUseCase } from "./interfaces/create-friend-invite-token.usecase.interface";
 export type { IApplyFriendUseCase } from "./interfaces/apply-friend.usecase.interface";
+export type { IGetUserFriendsUseCase } from "./interfaces/get-user-friends.usecase.interface";
