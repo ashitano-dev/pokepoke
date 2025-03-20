49945c2177150a7c9a691c6c27d86b67cfd8e872
koutyuke <gunji.kousuke08@gmail.com>
2025-03-21 03:28:10 +0900

    ♻️ refactor(application/use-cases/pack): update add and get friend pack use cases to utilize friendship logic

--- apps/backend/src/application/use-cases/pack/get-friend-pack.usecase.ts
+++ apps/backend/src/application/use-cases/pack/get-friend-pack.usecase.ts
@@ -1,13 +1,23 @@
 import { err } from "../../../common/utils";
 import type { UserId } from "../../../domain/value-object";
 import type { IPackRepository } from "../../../interface-adapter/repositories/pack";
+import type { IUserRepository } from "../../../interface-adapter/repositories/user";
 import type { GetFriendPackUseCaseResult, IGetFriendPackUseCase } from "./interfaces/get-friend-pack.usecase.interface";
 
 export class GetFriendPackUseCase implements IGetFriendPackUseCase {
-	constructor(private readonly packRepository: IPackRepository) {}
+	constructor(
+		private readonly packRepository: IPackRepository,
+		private readonly userRepository: IUserRepository,
+	) {}
 
 	public async execute(userId: UserId, friendId: UserId): Promise<GetFriendPackUseCaseResult> {
-		const pack = await this.packRepository.findByCreateUserIdAndTargetUserId(userId, friendId);
+		const friendship = await this.userRepository.findFriendshipByUserIds(userId, friendId);
+
+		if (!friendship) {
+			return err("NOT_FRIEND");
+		}
+
+		const pack = await this.packRepository.findByOwnerIdAndFriendshipId(userId, friendship.id);
 		if (pack === null) {
 			return err("NOT_FRIEND");
 		}
