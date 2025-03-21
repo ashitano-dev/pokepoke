10b69c4fa709c2094599078f337d3a5ec0aa4358
koutyuke <gunji.kousuke08@gmail.com>
2025-03-22 03:20:19 +0900

    📦️ build(apps/mobiles): configure SVG support and update dependencies

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -5,6 +5,21 @@ const { withNativeWind } = require("nativewind/metro");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+const { transformer, resolver } = config;
+
+config.transformer = {
+	...transformer,
+	babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
+};
+
+config.resolver = {
+	...resolver,
+	assetExts: resolver.assetExts.filter(ext => ext !== "svg"),
+	sourceExts: [...resolver.sourceExts, "svg"],
+	unstable_enablePackageExports: true,
+	unstable_conditionNames: ["browser", "require", "react-native"],
+};
+
 module.exports = withNativeWind(config, {
 	input: "./src/styles/global.css",
 	configPath: "tailwind.config.ts",
